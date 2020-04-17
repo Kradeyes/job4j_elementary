110b7bd900640a2e19b5f6b706175125500f274c
Kradeyes <yanin.r.a@mail.ru>
2020-04-17 15:43:06 +0300

    Разбор первой программы.[#275172]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Roman Yanin");
+        System.out.println("31.08.1990");
+    }
+}
