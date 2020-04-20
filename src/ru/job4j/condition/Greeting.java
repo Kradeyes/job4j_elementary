d206741baccdb9bdea44bbdb039c1a7e7499b1e1
Kradeyes <yanin.r.a@mail.ru>
2020-04-20 18:15:03 +0300

    9.1. String[#275163]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,9 +4,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        String ideafirstchange = idea + "But I am a newbie.";
+        idea = idea + "But I am a newbie.";
         int year = 2020;
-        String ideasecondchange = ideafirstchange + year;
-        System.out.println(ideasecondchange);
+        idea = idea + year;
+        System.out.println(idea);
     }
 }
