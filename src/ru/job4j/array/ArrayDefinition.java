948497769c44564f13ad355fb4af8b4f3a95f178
Kradeyes <yanin.r.a@mail.ru>
2020-04-21 20:11:50 +0300

    6.0.2. Заполнение массива.[#275141]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Alex Ivanov";
+        names[3] = "Max Ivanov";
+        for (String x:names) {
+            System.out.println(x);
+        }
+        }
     }
-}
+
