9dd1e5ba00d118a7a90f44649b705ab99db2acd9
Kradeyes <yanin.r.a@mail.ru>
2020-05-05 21:44:09 +0300

    1.3. Вызов метода с аргументами.[#275292]

---
+++ src/ru/job4j/inheritance/Profession.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class Profession {
+    String name;
+    String surname;
+    String education;
+    int birthday;
+    public String getName() {
+        return name;
+    }
+}
--- src/ru/job4j/oop/Jukebox.java
+++ src/ru/job4j/oop/Jukebox.java
@@ -3,9 +3,9 @@ package ru.job4j.oop;
 public class Jukebox {
     public void music(int position) {
         if (position == 1) {
-            System.out.println("Спят усталые игрушки, книжки спят." + '\n' + "Одеяла и подушки ждут ребят...");
+            System.out.println("Спят усталые игрушки, книжки спят." + System.lineSeparator() + "Одеяла и подушки ждут ребят...");
         } else if (position == 2) {
-            System.out.println("Пусть бегут неуклюже" + '\n' + "Пешеходы по лужам....");
+            System.out.println("Пусть бегут неуклюже" + System.lineSeparator() + "Пешеходы по лужам....");
         } else {
             System.out.println("Песня не найдена");
         }
