8ff286eded818c08048821c6f0481e725706cc3b
Kradeyes <yanin.r.a@mail.ru>
2020-05-07 19:06:05 +0300

    6. Метод замены заявки. Tracker.replace[#275311]

--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -5,23 +5,40 @@ import java.util.Objects;
 import java.util.Random;
 
 public class Tracker {
-
+    /**
+     * Массив для хранения заявок.
+     */
     private final Item[] items = new Item[100];
 
+    /**
+     * Указатель ячейки для новой заявки.
+     */
     private int position = 0;
 
+    /**
+     * Метод добавления заявки в хранилище
+     * @param item новая заявка
+     */
     public Item add(Item item) {
         item.setId(generateId());
         items[position++] = item;
         return item;
     }
 
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
     private String generateId() {
         Random rm = new Random();
         return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 
-
+    /**
+     * Метод выводит все заявки.
+     * @return массив всех не null элементов.
+     */
     public Item[] findAll() {
         Item[] findAll = new Item[items.length];
         int size = 0;
@@ -34,6 +51,11 @@ public class Tracker {
         return Arrays.copyOf(findAll, size);
     }
 
+
+    /**
+     * Метод реализует поиск заявок по имени.
+     * @return массив элементов совпавших по имени.
+     */
     public Item[] findByName(String key) {
         Item[] findByName = new Item[items.length];
         int size = 0;
@@ -46,16 +68,37 @@ public class Tracker {
         return Arrays.copyOf(findByName, size);
     }
 
+    /**
+     * Метод реализует поиск заявок по уникальному ключу.
+     * @return элемент по ключу.
+     */
     public Item findById(String id) {
-        Item byId = new Item("Test");
-        for (int index = 0; index < items.length; index++) {
-            Item item = this.items[index];
-            if (item.getId().equals(id)) {
-             byId = this.items[index];
-             break;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    /**
+     * Метод реализует поиск индекса по ключу.
+     * @return индекс.
+     */
+    private int indexOf(String id) {
+        int rsl = -1;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(id)) {
+                rsl = index;
+                break;
             }
         }
-        return byId;
+        return rsl;
+    }
+
+    /**
+     * Метод производит замену элемента по ключу.
+     */
+    public void replace(String id, Item item) {
+        int index = indexOf(id);
+        item.setId(id);
+        items[index].setName(item.getName());
     }
 
 }
--- test/ru/job4j/tracker/TrackerTest.java
+++ test/ru/job4j/tracker/TrackerTest.java
@@ -47,4 +47,15 @@ public class TrackerTest {
         expected[2] = item4;
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        tracker.add(bug);
+        String id = bug.getId();
+        Item bugWithDesc = new Item("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
+    }
 }
