f31d8c628664e9adce9fb66a347fa3247898a154
Kradeyes <yanin.r.a@mail.ru>
2020-05-17 18:58:36 +0300

    9.1 Рефакторинг теста @Before @After[#275248]

--- test/ru/job4j/stragery/PaintTest.java
+++ test/ru/job4j/stragery/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.stragery;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -9,19 +11,27 @@ import java.util.StringJoiner;
 
 public class PaintTest {
 
+    private final PrintStream stdout = System.out;
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
-        // проверяем результат вычисления
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringJoiner(System.lineSeparator())
                                 .add("+++++++")
@@ -31,7 +41,20 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringJoiner(System.lineSeparator())
+                                .add("  ++  ")
+                                .add(" ++++ ")
+                                .add("++++++")
+                                .toString()
+                )
+        );
     }
 }
