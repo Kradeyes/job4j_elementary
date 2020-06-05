87f1612ee4ed462237205ba50061b1d3af07ca85
Kradeyes <yanin.r.a@mail.ru>
2020-06-05 21:11:10 +0300

    2. Идентичные тексты.[212655#275243]

---
+++ src/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String x: origin) {
+            check.add(x);
+        }
+        for (String x: text) {
+            if (!check.contains(x)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
