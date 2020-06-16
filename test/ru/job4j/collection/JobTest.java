700ce2d072cf44ad8c46e8a867efc8d09a8cee73
Kradeyes <yanin.r.a@mail.ru>
2020-06-16 20:35:14 +0300

    2. Комбинированный компаратор.[10036#275226]#2

--- test/ru/job4j/collection/JobTest.java
+++ test/ru/job4j/collection/JobTest.java
@@ -1,22 +1,21 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class JobTest {
 
-    @Test
-    public void whenCompatorByNameAndPrority() {
-        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
-        int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
-        );
-        assertThat(rsl, lessThan(0));
-    }
+    private Job job1 = new Job("Impl task", 0);
+    private Job job2 = new Job("Fix bug", 1);
+    private Job job3 = new Job("Fix bug", 0);
 
     @Test
     public void whenAscByName() {
@@ -61,33 +60,40 @@ public class JobTest {
     }
 
     @Test
-    public void whenAscByNameAndPrority() {
+    public void whenAscByNameAndPriority() {
         Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
-        int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
-        );
-        assertThat(rsl, greaterThan(0));
+        List<Job> rsl = Arrays.asList(job1, job2, job3);
+        Collections.sort(rsl, cmpNamePriority);
+        List<Job> rslexp = Arrays.asList(job3, job2, job1);
+        assertEquals(rslexp, rsl);
     }
 
     @Test
-    public void whenAscByNameAndDescPrority() {
+    public void whenDescByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        List<Job> rsl = Arrays.asList(job1, job2, job3);
+        Collections.sort(rsl, cmpNamePriority);
+        List<Job> rslexp = Arrays.asList(job1, job2, job3);
+        assertEquals(rslexp, rsl);
+    }
+
+
+    @Test
+    public void whenAscByNameAndDescPriority() {
         Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobDescByPriority());
-        int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
-        );
-        assertThat(rsl, greaterThan(0));
+        List<Job> rsl = Arrays.asList(job1, job2, job3);
+        Collections.sort(rsl, cmpNamePriority);
+        List<Job> rslexp = Arrays.asList(job2, job3, job1);
+        assertEquals(rslexp, rsl);
     }
 
     @Test
-    public void whenDescByNameAndAscPrority() {
+    public void whenDescByNameAndAscPriority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobAscByPriority());
-        int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
-        );
-        assertThat(rsl, lessThan(0));
+        List<Job> rsl = Arrays.asList(job1, job2, job3);
+        Collections.sort(rsl, cmpNamePriority);
+        List<Job> rslexp = Arrays.asList(job1, job3, job2);
+        assertEquals(rslexp, rsl);
     }
 }
 
