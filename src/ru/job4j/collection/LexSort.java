266ebec306013c71a33382ec8be399ec4a62d603
Kradeyes <yanin.r.a@mail.ru>
2020-06-16 21:45:36 +0300

    4. Сортировка номера[221284#275224]

---
+++ src/ru/job4j/collection/LexSort.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] first = left.split(". ");
+        String[] second = right.split(". ");
+        Integer firstNum = Integer.parseInt(first[0]);
+        Integer secondNum = Integer.parseInt(second[0]);
+        return firstNum.compareTo(secondNum);
+    }
+}
